8be931a444705840f5d805dcc446e2cced5be157
coastalwhite <me@gburghoorn.com>
2026-05-04 22:25:43 +0200

    update flakes

--- configuration.nix
+++ configuration.nix
@@ -1,60 +1,70 @@
 {
-    www,
-    ...
+  pkgs,
+  www,
+  ...
 }:
 
 let
-    basedrive = "/dev/vda";
-    domain = "gburghoorn.com";
-in {
-    imports = [
-      ./hardware-configuration.nix
-    ];
-    
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  basedrive = "/dev/vda";
+  domain = "gburghoorn.com";
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-    # Use the GRUB 2 boot loader.
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = basedrive;
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-    services.openssh = {
-        enable = true;
-        settings = {
-            PasswordAuthentication = false;
-        };
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = basedrive;
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+    };
+  };
+
+  users.users = {
+    root = {
+      password = "pw123";
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuxKJCMup6I1t3QYBu1nUhIa1A2/zw8m3wrUFLNLpYn"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDg0iUGVVFPsP9NDU4hVY8NdP+HCokXmFxNyIZCxXo7s"
+      ];
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+
+    virtualHosts."${domain}" = {
+      enableACME = true;
+      forceSSL = true;
+      serverName = domain;
+      root = "${www}/";
     };
 
-    users.users = {
-        root = {
-            password = "pw123";
-            openssh.authorizedKeys.keys = [
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFuxKJCMup6I1t3QYBu1nUhIa1A2/zw8m3wrUFLNLpYn"
-                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDg0iUGVVFPsP9NDU4hVY8NdP+HCokXmFxNyIZCxXo7s"
-            ];
-        };
-    };
+    recommendedTlsSettings = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+  };
 
-    services.nginx = {
-        enable = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  security.acme.acceptTerms = true;
+  security.acme.certs = {
+    "${domain}".email = "me@gburghoorn.com";
+  };
 
-        virtualHosts."${domain}" = {
-            enableACME = true;
-            forceSSL = true;
-            serverName = domain;
-            root = "${www}/";
-        };
+  networking.firewall.allowedTCPPorts = [
+    22
+    80
+    443
+  ];
 
-		recommendedTlsSettings = true;
-		recommendedGzipSettings = true;
-		recommendedOptimisation = true;
-    };
-
-    security.acme.acceptTerms = true;
-    security.acme.certs = {
-        "${domain}".email = "me@gburghoorn.com";
-    };
-
-    networking.firewall.allowedTCPPorts = [ 22 80 443 ];
-
-	system.stateVersion = "23.11";
+  system.stateVersion = "26.05";
 }
